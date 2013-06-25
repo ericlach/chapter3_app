238a990d377f12f7e667373b965d158467a074aa
Eric Lachenmeier <elachenmeier@yahoo.com>
2013-06-25 11:22:31 -0700

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -60,6 +60,19 @@ describe "Authentication" do
         end
       end
 
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+
       #havins some trouble here, see listing 9.11
       describe "in the Users controller" do
 
